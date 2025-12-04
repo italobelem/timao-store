4c7cf11d010534fdaef2cbf6000fe3a61528ef27
Italo Belém <italobelem827@gmail.com>
2025-12-04 20:37:18 -0300

    feat: Adiciona tela de compra confirmada

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,13 @@ import { Routes } from '@angular/router';
 import { ProductListComponent } from './pages/product-list/product-list.component';
 import { ProductFormComponent } from './pages/product-form/product-form.component';
 import { CartComponent } from './pages/cart/cart.component';
+import { CheckoutSuccessComponent } from './pages/checkout-success/checkout-success.component';
 
 export const routes: Routes = [
     { path: 'products', component: ProductListComponent },
     { path: 'products/new', component: ProductFormComponent },
     { path: 'products/edit/:id', component: ProductFormComponent },
     { path: 'cart', component: CartComponent },
+    { path: 'checkout/success', component: CheckoutSuccessComponent },
+    { path: '**', redirectTo: '' }
 ];
