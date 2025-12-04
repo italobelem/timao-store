6c8f2f5de1b507a45956c0abe83e58004489b7c0
Italo Belém <italobelem827@gmail.com>
2025-12-04 20:48:06 -0300

    feat: Adiciona Landing Page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,8 +3,10 @@ import { ProductListComponent } from './pages/product-list/product-list.componen
 import { ProductFormComponent } from './pages/product-form/product-form.component';
 import { CartComponent } from './pages/cart/cart.component';
 import { CheckoutSuccessComponent } from './pages/checkout-success/checkout-success.component';
+import { HomeComponent } from './pages/home/home.component';
 
 export const routes: Routes = [
+      { path: '', component: HomeComponent },
     { path: 'products', component: ProductListComponent },
     { path: 'products/new', component: ProductFormComponent },
     { path: 'products/edit/:id', component: ProductFormComponent },
